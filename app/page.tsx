e5f6b56b3a9ab1b38f469451d96e645e6c88f87f
EmmanuelRBoo <EmmanuelRodriguesBoo@gmail.com>
2026-01-15 04:06:16 -0300

    feat: create 'Bloco 2'

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <Block id='motin_films'>
-        <h1 className='mb-8 font-light text-center text-4xl md:text-6xl lg:text-7xl '>
+        <h1 className='mb-8 font-light text-center text-4xl md:text-6xl lg:text-7xl'>
           Filmes de alto impacto
           <br />
           <span className='font-semibold'>com qualidade cinematográfica</span>
@@ -13,6 +13,17 @@ export default function Home() {
         <p className=' mb-12 text-xl md:text-2xl text-gray-300'>Criamos conteúdos que conectam marcas e pessoas através de histórias memoráveis</p>
 
         <button className='bg-primary min-w-72 w-1/2 py-2 whitespace-nowrap rounded-md cursor-pointer'>Conheça nossas soluções</button>
+      </Block>
+
+      <Block id='our_numbers'>
+        <h2 className='mb-8 font-semibold text-center text-2xl md:text-4xl lg:text-5xl'>Nossos números</h2>
+
+        <ul className='grid gap-6 grid-cols-1 lg:grid-cols-2'>
+          <li className='text-xl border border-zinc-600 w-96 p-2 rounded-md'><p>+10 anos de atuação</p></li>
+          <li className='text-xl border border-zinc-600 w-96 p-2 rounded-md'><p>+300 clientes satisfeitos</p></li>
+          <li className='text-xl border border-zinc-600 w-96 p-2 rounded-md'><p>+500 filmes registrados</p></li>
+          <li className='text-xl border border-zinc-600 w-96 p-2 rounded-md'><p>+2.000 projetos e filmes entregues</p></li>
+        </ul>
       </Block>
     </main>
   );
--- components/block/index.tsx
+++ components/block/index.tsx
@@ -4,7 +4,7 @@ export default function Block(props: BlockProps) {
     return (
         <section
             id={props.id}
-            className='w-screen h-screen flex items-center justify-center flex-col'
+            className='w-screen h-screen flex items-center justify-center flex-col border-t border-b border-zinc-600'
         >
             {props.children}
         </section>
